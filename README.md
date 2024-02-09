d85b00c23052bf3f3026c24997405e1d34f4a319
Juan J Cadima <juanjcadima@gmail.com>
2024-02-09 01:36:45 -0500

    lazyvim config for fullstack development

---
+++ README.md
@@ -0,0 +1,14 @@
+### 💤 LazyVim Installation
+
+For a complete guide to install LazyVim visit this <a target="_blank" href="https://www.lazyvim.org/installation">official guide</a>
+
+<br>
+
+
+Configuration development environment for Laravel, Node.js, s customised for Laravel, PHP, Tailwind, and JavaScript,
+
+
+
+
+
+
---
+++ lua/config/keymaps.lua
@@ -0,0 +1,13 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+-- change to normal mode with "jj"
+vim.keymap.set("i", "jj", "<ESC>", { noremap = true, silent = true, desc = "<ESC>" })
+
+-- Save file
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { noremap = true, desc = "Save window" })
+
+-- faster scrolling with  ALT+j, ALT+k 
+vim.keymap.set({ "n", "v" }, "<M-k>", "10k", { noremap = true, desc = "Up faster" })
+vim.keymap.set({ "n", "v" }, "<M-j>", "10j", { noremap = true, desc = "Down faster" })
